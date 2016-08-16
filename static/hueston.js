7fed16a6bf2a9322d43c766470aedba8f681efc6
Schlueter <bs@schlueter.blue>
2016-08-15 23:29:56 -0400

    remove unneeded methods; Indent better

--- static/hueston.js
+++ static/hueston.js
@@ -18,17 +18,13 @@ var Hueston = Hueston || function () {
 
       req.send()
     })
-    .then(response => JSON.parse(response))
-    .catch(error => console.log(error))
+      .then(response => JSON.parse(response))
+      .catch(error => console.log(error))
   }
 
   function getAPI(path, hubIP) {
     hubIP = hubIP || this.hubIP
     return get("http://" + hubIP + "/api/6c3N6aVAYAIvGrpe4uF9v3PR-l7udDtKDKX0veiQ/" + path)
-  }
-
-  function sleep(time) {
-    return new Promise(resolve => setTimeout(resolve, time))
   }
 
   this.hitTheLights = event => {
@@ -50,20 +46,20 @@ var Hueston = Hueston || function () {
   this.getHubIP = () => {
     return new Promise((resolve, reject) => {
       get("https://www.meethue.com/api/nupnp")
-      .then(
-        response => {
-          if (response.length === 1) {
-            this.hubID = response[0].id
-            this.hubIP = response[0].internalipaddress
-          } else {
-            // TODO
-            alert('Multi hub systems not yet supported')
-            reject('Multi hub systems not yet supported')
+        .then(
+          response => {
+            if (response.length === 1) {
+              this.hubID = response[0].id
+              this.hubIP = response[0].internalipaddress
+            } else {
+              // TODO
+              alert('Multi hub systems not yet supported')
+              reject('Multi hub systems not yet supported')
+            }
+            resolve(this.hubIP)
           }
-          resolve(this.hubIP)
-        }
-      )
-      .catch(error => console.log(error))
+        )
+        .catch(error => console.log(error))
     })
   }
 
@@ -71,29 +67,12 @@ var Hueston = Hueston || function () {
     return new Promise((resolve, reject) => {
       this.getHubIP().then(hubIP => {
         getAPI('lights', hubIP)
-        .then(response => {
-          this.lights = response
-          resolve(response)
-        })
-        .catch(error => console.log(error))
+          .then(response => {
+            this.lights = response
+            resolve(response)
+          })
+          .catch(error => console.log(error))
       })
     })
-  }
-
-  this.turnOn = lightid => {
-    httpRequest = new XMLHttpRequest();
-    httpRequest.open("PUT", "http://" + this.hubIP + "/api/6c3N6aVAYAIvGrpe4uF9v3PR-l7udDtKDKX0veiQ/lights/" + lightid + "/state", true)
-    httpRequest.send(JSON.stringify({
-      on: true,
-      sat: 254,
-      bri: 200,
-      hue: 50000
-    }))
-  }
-
-  this.turnOff = lightid => {
-    httpRequest = new XMLHttpRequest();
-    httpRequest.open("PUT", "http://" + this.hubIP + "/api/6c3N6aVAYAIvGrpe4uF9v3PR-l7udDtKDKX0veiQ/lights/" + lightid + "/state", true)
-    httpRequest.send(JSON.stringify({on: false}));
   }
 }
