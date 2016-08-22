7fe8a1ccbeba0d3f2a1a62e876dcc337f01c2c7c
Schlueter <bs@schlueter.blue>
2016-08-22 01:11:56 -0400

    Cleaning

--- static/main.js
+++ static/main.js
@@ -1,11 +1,9 @@
 (function () {
-'use strict';
-(function () {
+  'use strict';
   const logView = document.createElement('div')
   logView.id = 'logger'
-  logView.style = 'color: green; height: 100px; padding: 10px; overflow: scroll; background: black;'
+  logView.style = 'color: green; height: 500px; padding: 10px; overflow: scroll; background: black;'
   const body = document.getElementsByTagName('body')[0]
-  console.log(body.children[0])
   body.insertBefore(logView, body.children[0])
   window.log = statement => {
     logView.innerHTML += '<pre>' + statement
@@ -13,8 +11,9 @@
   }
 })()
 
+;(function () {
+'use strict';
 const prefixError = prefix => error => console.error(prefix, error)
-
 function loadScript(url) {
   return new Promise(function(resolve, reject) {
     const head = document.getElementsByTagName('head')[0]
