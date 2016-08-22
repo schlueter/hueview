efedcac2c4a88ce9163cb93d5506fe667bd1a6a9
Schlueter <bs@schlueter.blue>
2016-08-21 22:48:08 -0400

    Add bri,hue,sat,transitiontime to button data

--- static/hueview.js
+++ static/hueview.js
@@ -5,12 +5,22 @@ window.HueView = function(hueston) {
       const pollLights = window.setInterval(hueston.getLights, 2000)
       const setLight = event => hueston.updateLight(
         event.target.dataset.lightid,
-        {on: !hueston.lights[event.target.dataset.lightid].state.on}
+        {
+          on: !hueston.lights[event.target.dataset.lightid].state.on,
+          bri: parseInt(event.target.dataset.bri),
+          hue: parseInt(event.target.dataset.hue),
+          sat: parseInt(event.target.dataset.sat),
+          transitiontime: parseInt(event.target.dataset.transitiontime)
+        }
       )
       for (let lightid in hueston.lights) {
         if (hueston.lights.hasOwnProperty(lightid)) {
           const clicker = document.createElement('button')
           clicker.dataset.lightid = lightid
+          clicker.dataset.bri = 254
+          clicker.dataset.hue = 10000
+          clicker.dataset.sat = 254
+          clicker.dataset.transitiontime = 0
           clicker.innerHTML = hueston.lights[lightid].name
           clicker.onclick = setLight
           document.getElementById('hueview').appendChild(clicker)
