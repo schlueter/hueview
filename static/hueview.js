ee386ed6ed20ffdce8f71abb20c1ea98d6128576
Schlueter <bs@schlueter.blue>
2016-08-24 00:54:40 -0400

    Fuck make buttons

--- static/hueview.js
+++ static/hueview.js
@@ -1,33 +1,5 @@
 window.HueView = function(hueston) {
   'use strict';
-<<<<<<< a61b8c13b04e46093fed71120fa6294ca8856092
-  this.makeButtonsForLights = () =>
-    hueston.getLights().then(() => {
-      const setLight = event => hueston.updateLight(
-        event.target.dataset.lightid,
-        {
-          on: !hueston.lights[event.target.dataset.lightid].state.on,
-          bri: parseInt(event.target.dataset.bri),
-          hue: parseInt(event.target.dataset.hue),
-          sat: parseInt(event.target.dataset.sat),
-          transitiontime: parseInt(event.target.dataset.transitiontime)
-        }
-      )
-      for (let lightid in hueston.lights) {
-        if (hueston.lights.hasOwnProperty(lightid)) {
-          const clicker = document.createElement('button')
-          clicker.dataset.lightid = lightid
-          clicker.dataset.bri = 254
-          clicker.dataset.hue = 10000
-          clicker.dataset.sat = 254
-          clicker.dataset.transitiontime = 0
-          clicker.innerHTML = hueston.lights[lightid].name
-          clicker.onclick = setLight
-          document.getElementById('hueview').appendChild(clicker)
-        }
-      }})
-    .catch(Error)
-=======
 
   const setLight = event => hueston.updateLight(
     event.target.dataset.lightid,
@@ -71,7 +43,7 @@ window.HueView = function(hueston) {
     hueston.getLights()
       .then(lights => createLightControls(lights))
       .then(() => createAddNewLightButton())
-      .catch(error => Error(error))
+      .catch(Error)
+
   this.init()
->>>>>>> Break up view functions
 }
