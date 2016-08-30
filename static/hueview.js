aa305242befc76b24b5145d37ddbed0f60f30541
Schlueter <bs@schlueter.blue>
2016-08-30 00:26:39 -0400

    Populate lights from current values

--- static/hueview.js
+++ static/hueview.js
@@ -2,11 +2,11 @@ window.HueView = function(hueston) {
   'use strict';
   this.transitiontime = 0
 
-  const createLightControl = lightid => {
+  const createLightControl = (lightid, settings) => {
     const control = document.createElement('div')
 
     const title = document.createElement('h3')
-    title.textContent = hueston.lights[lightid].name
+    title.textContent = settings.name
     control.appendChild(title)
 
     const toggle = document.createElement('button')
@@ -18,20 +18,20 @@ window.HueView = function(hueston) {
     deleteLight.onclick = () => hueston.deleteLight(lightid)
     control.appendChild(deleteLight)
 
-    const lightSettings = {}
+    const lightSettings = settings
 
     const inputConfigs = [
-      {name: 'bri', placeholder: 'brightness', min: 0, max: 254},
-      {name: 'sat', placeholder: 'saturation', min: 0, max: 254},
-      {name: 'hue', placeholder: 'hue'       , min: 0, max: 65535}
+      {name: 'bri', min: 0, max: 254},
+      {name: 'sat', min: 0, max: 254},
+      {name: 'hue', min: 0, max: 65535}
     ]
 
     inputConfigs.forEach(config => {
       const input = document.createElement('input')
       input.placeholder = config.placeholder
+      input.defaultValue = settings.state[config.name]
       input.oninput = event => lightSettings[config.name] = parseInt(event.target.value)
       input.onkeydown = event => {
-        console.log(event)
         if (event.key === "Enter") {
           lightSettings.on = true
           hueston.updateLight(lightid, lightSettings)
@@ -40,14 +40,13 @@ window.HueView = function(hueston) {
       control.appendChild(input)
     })
 
-    window.control = control
     document.getElementById('hueview').appendChild(control)
   }
 
   const createLightControls = lights => {
     for (let lightid in lights) {
       if(/^(\-|\+)?([0-9]+)$/.test(lightid)) {
-        createLightControl(lightid)
+        createLightControl(lightid, lights[lightid])
       }
     }
   }
