58ef7658a3bd6886d2a63ce13b44d60ac312c214
Schlueter <bs@schlueter.blue>
2016-09-27 22:45:32 -0400

    Adapt Mozilla's draggable div code

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,38 +1,29 @@
 (function () {
-'use strict';
-const makeSlider = parent => {
-  const verticalBar = document.createElement('div')
-  verticalBar.classList.add('slider-vertical-bar')
-  verticalBar.style = 'height: 100%; width: 20px; background: black; margin-left: auto; margin-right: auto'
-  const horizontalBar = document.createElement('div')
-  horizontalBar.style = 'height: 20px; width: 100%; position: absolute; background: black; margin-top: auto; margin-bottom: auto; top: 0'
-  horizontalBar.classList.add('slider-horizontal-bar')
-  horizontalBar.onmousedown = mouseDownEvent => {
-    var position = mouseDownEvent.y
-    event.target.onmousemove = mouseMoveEvent => {
-      const movement = position - mouseMoveEvent.y
-      position = mouseMoveEvent.y
-      const newPosition = mouseMoveEvent.target.offsetTop - movement
-      if (newPosition >= verticalBar.offsetTop - horizontalBar.offsetHeight / 2 &&
-          newPosition <= verticalBar.offsetTop + verticalBar.offsetHeight - horizontalBar.offsetHeight / 2) {
-        console.log(newPosition / verticalBar.offsetHeight)
-        mouseMoveEvent.target.style.top = newPosition
-      }
-    }
-  }
-  const wrapper = document.createElement('div')
-  wrapper.classList.add('slider-wrapper')
-  wrapper.style = 'width: 100px; position: relative'
-  wrapper.appendChild(verticalBar)
-  wrapper.appendChild(horizontalBar)
-  parent.appendChild(wrapper)
-  document.onmouseup = mouseUpEvent => horizontalBar.onmousemove = null
-}
+  'use strict';
+  var slider, initialY, movementY
+  var bMouseUp = true
 
-const sliders = document.getElementsByClassName('slider')
-for (var slider in sliders) {
-  if (sliders.hasOwnProperty(slider)) {
-    makeSlider(sliders[slider])
+  document.onmouseup = () => bMouseUp = true
+  document.onmousemove = onmousemoveEvent => bMouseUp ? false
+    : slider.style.top = String(movementY + onmousemoveEvent.clientY - initialY) + 'px'
+  document.onmousedown = onmousedownEvent => {
+    var bExit = true
+    for (var iNode = onmousedownEvent.target; iNode; iNode = iNode.parentNode) {
+      if (iNode.className === 'slider') {
+  			bExit = false
+  			slider = iNode
+  			break
+  		}
+    }
+    if (bExit) {
+  	  return
+  	}
+    bMouseUp = false
+    movementY = 0
+    for (var iOffPar = slider; iOffPar; iOffPar = iOffPar.offsetParent) {
+      movementY += iOffPar.offsetTop
+    }
+    initialY = onmousedownEvent.clientY
+    return false
   }
-}
 })()
