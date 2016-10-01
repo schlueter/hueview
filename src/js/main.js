f456de69c60fa45fe78c203f32f1bb27e8ba48f5
Schlueter <bs@schlueter.blue>
2016-10-01 12:30:34 -0400

    Kill magic script loader

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,4 @@
 (function () {
-'use strict';
-Promise.all([
-  'static/hueston.js',
-  'static/hueview.js',
-  'static/slider.js',
-].map(url => new Promise(function(resolve, reject) {
-    const head = document.getElementsByTagName('head')[0]
-    const script = document.createElement('script')
-    script.src = url
-    script.async = false
-    script.onload = resolve
-    script.onerror = reject
-    head.appendChild(script)
-})))
-  .then(() => new HueView(new Hueston()))
-  .catch(error => console.error('Error loading scripts:', error))
+  'use strict';
+  new HueView(new Hueston())
 })()
